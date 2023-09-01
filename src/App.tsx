f380688191a5ddad613349f3614bec3b6c6ba6b9
Kyle Karpack <karpack@adobe.com>
2023-09-01 14:34:54 -0700

    Fix merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,6 @@ const imgs = [
   "https://www.nwrfc.noaa.gov/weather/plots/2023/20230901/QPF_24_DAY6_20230907_12_20230901203026_chps_webplots.png",
 ];
 
-const imgs = [
-  "https://www.nwrfc.noaa.gov/weather/plots/2023/20230901/QPF_24_DAY1_20230902_12_20230901150114_chps_webplots.png",
-  "https://www.nwrfc.noaa.gov/weather/plots/2023/20230901/QPF_24_DAY2_20230903_12_20230901150115_chps_webplots.png",
-  "https://www.nwrfc.noaa.gov/weather/plots/2023/20230901/QPF_24_DAY3_20230904_12_20230901150116_chps_webplots.png",
-  "https://www.nwrfc.noaa.gov/weather/plots/2023/20230901/QPF_24_DAY4_20230905_12_20230901150117_chps_webplots.png",
-];
-
 function App() {
   return (
     <div className="App">
